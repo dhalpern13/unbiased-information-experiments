d129af814061df461d0f28433760d6b5ce29eaf7
Daniel Halpern <dhalpern13@gmail.com>
2021-01-29 16:08:37 -0500

    Add plots

--- graph_samples.py
+++ graph_samples.py
@@ -2,18 +2,7 @@ from itertools import product
 
 import matplotlib.pyplot as plt
 
-from experiments import distributions, generate_xs_ys
-
-noise_dists = [
-    'uniform(-1,1)',
-    'normal(0,.5^2)',
-    'beta(2-2)-mirrored',
-    'power-law(2)-mirrored'
-]
-
-x_distribution = distributions['uniform(-2,2)']
-
-noise_distributions = {name: dist for name, dist in distributions.items() if name in noise_dists}
+from experiments import noise_distributions, x_distribution, generate_xs_ys
 
 slopes = [0, 1, 5]
 
